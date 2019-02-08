86f520e42b07eb0cb2b25ef3ef274fb40283ee2f
Dan_Y <daniel.yd.liu@gmail.com>
2019-02-07 23:56:17 -0800

    First commit: maximizes area for catching rain!

--- trappingRainWater.java
+++ trappingRainWater.java
@@ -1,14 +1,16 @@
 class Solution {
     public int maxArea(int[] height) {
-        int hi = height.length-1;
+        int hi = height.length - 1;
         int lo = 0;
         int a = 0;
+
         while(lo != hi) {
-            int currentArea = Math.min(height[hi], height[lo])*(hi-lo);
+            int currentArea = Math.min(height[hi], height[lo])*(hi - lo);
             if (height[lo] < height[hi]) lo++;
             else hi--;
             if (currentArea > a) a = currentArea;
         }
+
         return a;
     }
 }
